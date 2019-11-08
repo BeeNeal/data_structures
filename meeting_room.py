261b1359d49d1c98a526576d77e93c0b1b9d4740
BNeal22 <brittanyneal22@gmail.com>
2019-11-07 18:25:16 -0800

    solved meeting room problem with min heap

---
+++ meeting_room.py
@@ -0,0 +1,41 @@
+# min heap - binary tree where root and each node is <= child nodes.
+
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class NumberOfMeetingRooms(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+
+        # If there is no meeting to schedule then no room needs to be allocated.
+        if len(intervals) < 1:
+            return 0
+
+        # initialize the heap
+        free_rooms = []
+
+        # Sort the meetings in increasing order of their start time.
+        intervals.sort(key= lambda x: x.start)
+
+        # Add the first meeting. First meeting must take a room.
+        heapq.heappush(free_rooms, intervals[0].end)
+
+        # For all the remaining meeting rooms
+        for i in intervals[1:]:
+
+            # If the room due to free up the earliest is free, assign that room to this meeting.
+            if free_rooms[0] <= i.start:
+                heapq.heappop(free_rooms)
+
+            # If a new room is to be assigned, then add to the heap,
+            # If an old room is allocated, then add to the heap with updated end time.
+            heapq.heappush(free_rooms, i.end)
+
+        # The size of the heap tells us the minimum rooms required for all the meetings.
+        return len(free_rooms)
