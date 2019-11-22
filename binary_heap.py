a227f79e43642ae6a407a2b8088d4b8b68b74621
BeeNeal <brittanyneal22@gmail.com>
2019-11-21 22:51:10 -0800

    code stub for ll integer addition problem

--- binary_heap.py
+++ binary_heap.py
@@ -1,13 +1,24 @@
 # A property of a complete tree is that can represent with a single list
 # in a complete tree, the left child of a parent (at position p) is found in
 #  position 2p and right child is 2p + 1
+# node at position n means parent is position n/2
 
 class BinaryHeap:
     """Implement a balanced Binary Tree"""
 
+    def __init__(self):
+        """Initialize binary heap with single list."""
+
+        # inititalize with a zero so no errors with integer division
+        self.heapList = [0]
+        self.currentSize = 0
+    
+
     def insert(self, item):
         """adds a new item to the heap"""
 
+        # since is list, could append, however that would violate the heap,
+        # so need to do an insertion sort type insertion to insert
         pass
 
     def findMin():
@@ -28,7 +39,7 @@ class BinaryHeap:
     def size():
         """returns the number of items in the heap"""
 
-        pass
+        return self.currentSize
 
     def buildHeap(list):
         """builds a new heap from a list of keys"""
