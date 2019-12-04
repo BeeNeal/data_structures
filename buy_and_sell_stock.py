45050bcc5b5665c6931f965ad868a65fdd1224a3
BeeNeal <brittanyneal22@gmail.com>
2019-12-04 13:39:18 -0800

    added doctest

--- buy_and_sell_stock.py
+++ buy_and_sell_stock.py
@@ -1,21 +1,24 @@
 def max_stock_profit(prices):
-    """ """
+    """Take in list of stock prices, return max profit possible from 1 trade
+    
+    >>> max_stock_profit([7,1,5,3,6,4])
+    5
+    >>> max_stock_profit([2,3])
+    1
+    """
 
     min_price_seen = float('inf')
     max_profit = 0
 
-    # keep track of the min price
-    # keep track of the max difference
     for price in prices:
         profit_today = price - min_price_seen
-        max_profit = max(max_profit, price - min_price_seen)
+        max_profit = max(max_profit, profit_today)
         min_price_seen = min(min_price_seen, price)
-        
+
     return max_profit
 
 
-
-
-prices = [7,1,5,3,6,4]
-# 5
-print(max_stock_profit(prices))
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod():
+        print("max profit!")
