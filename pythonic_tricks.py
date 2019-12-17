f3ebb8c569433b6fb0c36ca5c00b5ab5c523c01b
BeeNeal <brittanyneal22@gmail.com>
2019-12-16 18:46:24 -0800

    added finding substring and chunk functions to pythonictricks

--- pythonic_tricks.py
+++ pythonic_tricks.py
@@ -1,3 +1,7 @@
+from math import floor, ceil
+
+
+
 def count_by(arr, fn=lambda x: x):
     """Counts elements in array by whatever function given."""
 
@@ -17,8 +21,27 @@ def alt_word_score(word):
     return sum([ord(c) for c in word])
 
 
-from math import floor
 print(count_by([8.1, 8.4, 9.0, 5.2, 5, 6, 8.2], floor))
 print(count_by(['one', 'two', 'three', 'four'], len))
 print(count_by(['abba', 'baba', 'bobo', 'bbaa'], word_score))
 # demonstrates lambda, and passing function into function
+
+for word in ['brittany', 'neal', 'omar', 'shishani']:
+    print(len(word))
+
+
+
+def chunk(lst, size):
+    """Take in list and return a list of lists of specified size."""
+
+    # math.ceil rounds up to greatest integer value (-13.1 -> -13, 103.4 -> 104)
+    return list(
+        map(lambda x: lst[x * size:x * size + size],
+            list(range(0, ceil(len(lst) / size)))))
+
+
+def find_substring(test_str, sub):
+    res = [i for i in range(len(test_str)) if test_str.startswith(sub, i)]
+    return res
+
+print(find_substring('omarshishaniisinthissstringomarishere omar is here', 'omar'))
