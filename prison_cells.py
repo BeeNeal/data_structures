59e0f5dd9e1d7fe72bacdfe7c2eff2d3ae5f5097
BeeNeal <brittanyneal22@gmail.com>
2019-12-19 08:52:33 -0800

    working solution using dictionary to capture patterns, and can run 10^9

--- prison_cells.py
+++ prison_cells.py
@@ -22,17 +22,40 @@ class Prison:
         """
 
         self.cells = cells
+        self.prisonDays = dict()
+        self.prisonDays[0] = self.cells
+        # self.pattern = dict()
+
+    # def findPattern(self, d):
+    #     """Find the cycle, store cell day as key. SCRIPT"""
+
+    #     tupled_day = tuple(self.cells)
+    #     if tupled_day in self.pattern:
+    #         print("PATTERN REPEATS AT DAY = {}".format(self.pattern[tupled_day]))
+    #         return "PATTERN REPEATS AT DAY = {}".format(self.pattern[tupled_day])
+    #     self.pattern[tupled_day] = d
+
+    def showDays(self, days):
+        """Store prison day pattern with day as key."""
+
+        if days > 14:
+            days = days % 14
+        return self.prisonDays[days]
+        # if day == 0:
+        #     day = 14
+        # repeats after 14
+
 
     def prisonAfterNDays(self, days):
-        """ """
+        """Populate a dictionary with patterns of days - 14 b/c 8 cells """
 
-        self.prisonAfterDay = dict()
-
-        for d in range(days):
-            # print(self.cells)
+        for d in range(1, 14):
             self.fill_prison(self.cells)
-            self.prisonAfterDay[tuple(self.cells)] = d
-        
+            self.prisonDays[d] = self.cells
+        print(self.prisonDays)
+
+            # self.findPattern(d) 
+
         return self.cells
 
     def fill_prison(self, cells):
@@ -49,18 +72,18 @@ class Prison:
                 new_cells.append(0)
         new_cells.append(0)
 
-
         self.cells = new_cells
 
 new_prison = Prison([0, 1, 0, 1, 1, 0, 0, 1])
-print(new_prison.prisonAfterNDays(90))
+print(new_prison.prisonAfterNDays(7))
 
 # above solution works great until there are billions of days... 
 # takes several minutes to run 1000000000
 # there must be a pattern, where the cycle repeats. 
-# Use hashmap to find pattern -> store the final
+# Use hashmap to find pattern -> store each iteration and access through keys
 
 # lessons learned:
 # OOP to re-use cells, passing into function call
 # run a function call multiple times with range
 # formatting long conditional statements to adhere to pep8 with () and whitespace
+
